02998b2007450d4cfaa7cf7cc123c44d483e570c
bobtsui <grimmo58968@gmail.com>
2017-09-16 21:37:16 +0800

    001-第一版

---
+++ 直通(每次執行都打亂陣列)/直通(每次執行都打亂陣列)/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 直通_每次執行都打亂陣列_
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            this.label1.Text = "";
+            Random random = new Random();
+            int test;
+            for (int i = 0; i < 10; i++)
+            {
+                int idx = random.Next(i, 10);  //隨機
+                //swap elements
+                test = array[i]; 
+                array[i] = array[idx];
+                array[idx] = test;
+                
+            }
+            foreach (var a in array)
+            {
+                string ab = a + " ";
+                this.label1.Text += ab;
+            }
+
+        }
+
+    }
+}
---
+++ 直通(每次執行都打亂陣列)/直通(每次執行都打亂陣列)/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 直通_每次執行都打亂陣列_
+{
+    static class Program
+    {
+        /// <summary>
+        /// 應用程式的主要進入點。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
