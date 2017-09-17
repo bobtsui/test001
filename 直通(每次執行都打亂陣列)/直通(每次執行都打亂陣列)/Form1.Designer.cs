1e382348556266118100d39f981152ee3849d2c4
bobtsui <grimmo58968@gmail.com>
2017-09-17 22:05:30 +0800

    005-在板橋弄

--- 直通(每次執行都打亂陣列)/直通(每次執行都打亂陣列)/Form1.Designer.cs
+++ 直通(每次執行都打亂陣列)/直通(每次執行都打亂陣列)/Form1.Designer.cs
@@ -57,9 +57,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(45, 203);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(33, 12);
+            this.label2.Size = new System.Drawing.Size(87, 12);
             this.label2.TabIndex = 2;
-            this.label2.Text = "label2";
+            this.label2.Text = "label2000000000";
             this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // Form1
